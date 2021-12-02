9de1a4b90b0263e952b047104c8c7fdf5a433408
Joel Wetzell <jwetzell@yahoo.com>
2021-12-02 09:54:13 -0600

    Start adding location awareness to timezone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
-import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 import { interval, Observable } from 'rxjs';
-import { Timezone } from './timezone/models/timezone.model';
+import { startWith } from 'rxjs/operators';
+import { GetClosestTimezoneFrom, Timezone } from './timezone/models/timezone.model';
 import { TimezoneService } from './timezone/services/timezone.service';
-
-
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -21,16 +17,16 @@ export class AppComponent {
 
   fiveOclocks: Timezone[] = [];
 
-  constructor(private timezoneService: TimezoneService){
-    this.second = interval(1000)
+  constructor(private timezoneService: TimezoneService) {
+    this.second = interval(1000).pipe(startWith(0)) //every second and give it a starter value
 
-    this.second.subscribe(()=>{
+    this.second.subscribe(() => {
       this.fiveOclocks = [];
-      this.timezoneService.getCurrentTimeInTimezones(this.timezoneService.getTimezones()).forEach((timezoneObj)=>{
-        if(timezoneObj.time?.hour === 17){
-          this.fiveOclock = timezoneObj;
+      this.timezoneService.getCurrentTimeInTimezones(this.timezoneService.getTimezones()).forEach((timezoneObj) => {
+        if (timezoneObj.time?.hour === 17) {
           this.fiveOclocks.push(timezoneObj);
         }
+        this.fiveOclock = GetClosestTimezoneFrom(this.timezoneService.getCurrentTimezone(), this.fiveOclocks)
       })
     })
 
