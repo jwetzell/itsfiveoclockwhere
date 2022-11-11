40074f88ead67aab27b88b67dd482913b10cfd81
Joel Wetzell <jwetzell@cait.org>
2022-11-11 10:03:27 -0600

    remove unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,11 @@ import { TimezoneService } from './timezone/services/timezone.service';
 import { AppRoutingModule } from './app-routing.module';
 import { TimezoneListComponent } from './timezone/components/timezone-list/timezone-list.component';
 import { FiveoclockComponent } from './fiveoclock/fiveoclock.component';
-import { TimezoneSingleComponent } from './timezone/components/timezone-single/timezone-single.component';
 import { GoogleAnalyticsService } from './services/google-analytics.service';
 @NgModule({
   declarations: [
     AppComponent,
     TimezoneListComponent,
-    TimezoneSingleComponent,
     FiveoclockComponent
   ],
   imports: [
