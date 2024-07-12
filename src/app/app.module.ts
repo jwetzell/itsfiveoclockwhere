a5a0a84f75736158f18ac6a7498ecfd697a59809
Joel Wetzell <jwetzell@yahoo.com>
2024-07-11 20:20:09 -0500

    Remove Google analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { GoogleAnalyticsService } from './services/google-analytics.service';
 import { TimezoneListComponent } from './timezone/components/timezone-list/timezone-list.component';
 import { TimezoneService } from './timezone/services/timezone.service';
 @NgModule({
   declarations: [AppComponent, TimezoneListComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
-  providers: [TimezoneService, GoogleAnalyticsService],
+  providers: [TimezoneService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
